56428af2136ef995481023d31734df076dda6f93
Kyle Erikson <kderikson@gmail.com>
2015-04-30 16:36:25 -0400

    Delete testterm.py

--- testterm.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from subprocess import Popen, PIPE
-import os
-
-sudoPassword = '19!jobs!76'
-command = 'sudo apt-get install apache2'
-p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
